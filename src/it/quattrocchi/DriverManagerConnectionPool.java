c9a7b6db16ff8b8b9653d8d6cb1a3795d7956281
BuildTools <unconfigured@null.spigotmc.org>
2017-05-10 18:04:07 +0200

    per il db

--- src/it/quattrocchi/DriverManagerConnectionPool.java
+++ src/it/quattrocchi/DriverManagerConnectionPool.java
@@ -23,7 +23,7 @@ public class DriverManagerConnectionPool  {
 		Connection newConnection = null;
 		String ip = "localhost";
 		String port = "3306";
-		String db = "QuattrocchiDB";
+		String db = "quattrocchidb";
 		String username = "progetto";
 		String password = "pw";
 
