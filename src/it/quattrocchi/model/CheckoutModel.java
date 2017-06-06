21a0a3ad4524536b847d87481fb15a057a7be83a
abbettino <albertinopalmiero@gmail.com>
2017-06-06 15:52:23 +0200

    piano di lavoro per i prossimi giorni

---
+++ src/it/quattrocchi/model/CheckoutModel.java
@@ -0,0 +1,16 @@
+package it.quattrocchi.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+public class CheckoutModel {
+private static final String TABLE_NAME = "quattrocchidb.ordine";
+	
+	public void doSave() throws SQLException{ //bisogna salvare il pataterno, forse bisogna creare dei bean
+		
+		
+	}
+
+}
