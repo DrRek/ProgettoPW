03169bb8032da5d47df901e3154530aa8fb7d464
abbettino <albertinopalmiero@gmail.com>
2017-06-11 20:10:29 +0200

    convertito javscript in jquery ma continua a non funzionare

--- WebContent/js/access-validation.js
+++ WebContent/js/access-validation.js
@@ -1,45 +1,24 @@
 function registerValidation()
 {
-	var uid = document.registration.user;
-	var pass = document.registration.pass;
-	var uname = document.registration.nome;
-	var ulastname = document.registration.cognome;
-	var uadd = document.registration.indirizzo;
-	var ucountry = document.registration.stato;
-	var ucap = document.registration.cap;
-	var uemail = document.registration.email;
+	var uid = $("input[name='user']");
+	var pass = $("input[name='pass']");
+	var uname = $("input[name='nome']");
+	var ulastname =  $("input[name='cognome']");
+	var uadd =  $("input[name='indirizzo']");
+	var ucountry =  $("select[name='stato']");
+	var ucap =  $("input[name='cap']");
+	var uemail =  $("input[name='email']");
 
-	if(user_validation(uid,5,12))
-	{
-		if(pass_validation(pass,7,12))
-		{
-			if(allLetter(uname))
-			{
-				if(allLetter(ulastname))
-				{
-					if(alphanumeric(uadd))
-					{ 
-						if(countryselect(ucountry))
-						{
-							if(allnumeric(ucap))
-							{
-								if(ValidateEmail(uemail))
-								{
-									return true;
-								} 
-							}
-						} 
-					}
-				}
-			}
-		}
-	}
-	return false;
+	if(user_validation(uid,5,12) && pass_validation(pass,7,12) && allLetter(uname) && allLetter(ulastname) 
+			&& alphanumeric(uadd) && countryselect(ucountry) && allnumeric(ucap) && ValidateEmail(uemail))
+		return true;
+	else 
+		return false;
 } 
 
 function user_validation(uid,mx,my)
 {
-	var uid_len = uid.value.length;
+	var uid_len = uid.val().length;
 	if (uid_len == 0 || uid_len >= my || uid_len < mx)
 	{
 		$("#user").html("Username non dovrebbe essere vuoto, lunghezza tra "+mx+" e "+my);
@@ -50,7 +29,7 @@ function user_validation(uid,mx,my)
 }
 function pass_validation(pass,mx,my)
 {
-	var pass_len = pass.value.length;
+	var pass_len = pass.val().length;
 	if (pass_len == 0 ||pass_len >= my || pass_len < mx)
 	{
 		$("#pass").html("Password non dovrebbe essere vuoto, lunghezza tra "+mx+" e "+my);
@@ -62,10 +41,8 @@ function pass_validation(pass,mx,my)
 function allLetter(uname)
 { 
 	var letters = /^[A-Za-z]+$/;
-	if(uname.value.match(letters))
-	{
+	if(uname.val().match(letters))
 		return true;
-	}
 	else
 	{
 		$("#nome").html("È consentito usare solo caratteri alfabetici");
@@ -76,10 +53,8 @@ function allLetter(uname)
 function alphanumeric(uadd)
 { 
 	var letters = /^[0-9a-zA-Z ]+$/;
-	if(uadd.value.match(letters))
-	{
+	if(uadd.val().match(letters))
 		return true;
-	}
 	else
 	{
 		$("#indirizzo").html("L'indirizzo può solo contenere caratteri alfanumerici");
@@ -89,24 +64,20 @@ function alphanumeric(uadd)
 }
 function countryselect(ucountry)
 {
-	if(ucountry.value == "Default")
+	if(ucountry.val() == "Default")
 	{
 		$("#stato").html("Seleziona il tuo paese dalla lista");
 		ucountry.focus();
 		return false;
 	}
 	else
-	{
 		return true;
-	}
 }
 function allnumeric(ucap)
 { 
 	var numbers = /^[0-9]+$/;
-	if(uzip.value.match(numbers))
-	{
+	if(uzip.val().match(numbers))
 		return true;
-	}
 	else
 	{
 		$("#cap").html("Il codice postale può contenere solo numeri");
@@ -117,10 +88,8 @@ function allnumeric(ucap)
 function ValidateEmail(uemail)
 {
 	var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-	if(uemail.value.match(mailformat))
-	{
+	if(uemail.val().match(mailformat))
 		return true;
-	}
 	else
 	{
 		$("#email").html("Email non valida");
